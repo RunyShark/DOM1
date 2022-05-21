2e982bf03ce7cf58b8f95f4121bc6e67bf608564
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-05-20 21:18:32 -0500

    50%

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const carrito = document.querySelector("#carrito");
 const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarritoBn = document.querySelector("#vaciar-carrito");
 const listaCurso = document.querySelector("#lista-cursos");
+let articulos = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
@@ -11,6 +12,38 @@ function cargarEventListeners() {
 function addCurso(e) {
   e.preventDefault();
   if (e.target.classList.contains("agregar-carrito")) {
-    console.log(e.target);
+    const add = e.target.parentElement.parentElement;
+    leerDatos(add);
   }
+}
+
+function leerDatos(curso) {
+  const infoCurso = {
+    id: curso.querySelector("a").getAttribute("data-id"),
+    imagen: curso.querySelector("img").src,
+    nombre: curso.querySelector("h4").textContent,
+    precio: curso.querySelector(".precio span").textContent,
+    cantidad: 1,
+  };
+
+  articulos = [infoCurso, ...articulos];
+  carritoHTML();
+}
+
+function carritoHTML() {
+  deleteHTML();
+  articulos.forEach((e) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+    <td>
+    ${e.nombre}
+   
+    
+    `;
+    contenedorCarrito.appendChild(row);
+  });
+}
+
+function deleteHTML() {
+  contenedorCarrito.innerHTML = "";
 }
